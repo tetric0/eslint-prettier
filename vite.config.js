0b566b5bbe5dbd2dc2555c9eb902a31893f91c2b
victor.munoz <victor.munoz@upc.edu>
2024-03-12 10:42:52 +0100

    Arregla import de 'fileURLToPath' en fichero vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLTopath(new URL('./src/', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src/', import.meta.url)),
+    },
+  },
+});
